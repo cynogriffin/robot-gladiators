cdf0b6691af515c1b6c5f118b2e834ad828fdfcc
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2021-12-06 00:33:02 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,8 +68,18 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  // call fight function with enemy robot
-  fight(pickedEnemyName);
+  if (playerHealth > 0) {
+    // let player know what round they are in
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+    // pick new enemy to fight based on the index of the enemyNames
+    var pickedEnemyName = enemyNames[i];
+    //reset enemyHealth before starting new fight
+    enemyHealth = 50;
+    // debugger;
+    // call fight function with pickedEnemyName variable value
+    fight(pickedEnemyName);
+  } else {
+    window.alert("You have lost your robot in battle! Game over!");
+    break;
+  }
 }
