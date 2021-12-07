06a20d6ac73cbffb0c156cc79f67a0d7440a67a6
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2021-12-07 00:15:25 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,7 +22,7 @@ var fight = function(enemyName) {
       if (confirmSkip) {
           window.alert(playerName + " has decided to skip this fight. Goodbye!");
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney);
           break;
       };
@@ -31,9 +31,11 @@ var fight = function(enemyName) {
     
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
-      
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
       );
@@ -54,9 +56,11 @@ var fight = function(enemyName) {
     } else {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     };
-  
+    
     // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
@@ -89,7 +93,7 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
       
       //reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       
       // debugger;
       
@@ -127,6 +131,7 @@ var endGame = function() {
     window.alert("You've lost your robot in battle.");
   }
   window.alert("The game has now ended. Let's see how you did!");
+  
   // ask the player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
@@ -187,6 +192,13 @@ var shop = function() {
       break;
   }
 };
+
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+}
 
 // start the game when the page loads
 startGame();
